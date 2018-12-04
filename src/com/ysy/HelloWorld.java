24371475c85f4f1aeea6126e832dea6d4c18e747
Administrator <525996609@qq.com>
2018-12-04 21:13:51 +0800

    cs

--- src/com/ysy/HelloWorld.java
+++ src/com/ysy/HelloWorld.java
@@ -2,6 +2,6 @@ package com.ysy;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("hello world");
+        System.out.print("hello world6578568");
     }
 }
